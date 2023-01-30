c819df5998ae51d29a537c75e4dc85a32c2c52f9
Antoine07 <antoine.lucsko@gmail.com>
2023-01-30 17:59:13 +0100

    correction exo 06 permutation

--- J1_2_3_introduction_structure_donnees/Exercices/06_bis_permutation.js
+++ J1_2_3_introduction_structure_donnees/Exercices/06_bis_permutation.js
@@ -1,17 +1,14 @@
-function swap(a, b, c) {
-  let d = c;
-  c = b;
-  b = a;
-  a = d;
-
-  return [a, b, c];
-}
-
-let [u, i, v] = swap(1, 2, 3);
-
-console.log(u, i, v);
-
-// On peut assigner des variables en utilisant des tableaux 
+// On peut assigner des variables en utilisant des tableaux
 let [a, b, c] = [1, 2, 3];
 
+[c, a, b] = [a, b, c];
+
 console.log(a, b, c);
+
+function permutation(a, b, c){
+    [c, a, b] = [a, b, c];
+
+    return {a, b, c};
+}
+
+console.log(permutation(1, 2, 3));
